1a838cf542f6bfb4bd7a49bcc1a6a2a7da09ef7d
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-05-11 16:41:07 +0900

    Update A05_LongestPalindromSubstring.java

--- 02/A05_LongestPalindromSubstring.java
+++ 02/A05_LongestPalindromSubstring.java
@@ -5,31 +5,42 @@ public class A05_LongestPalindromSubstring {
 	public static void main(String[] args) {
 		A05_LongestPalindromSubstring a = new A05_LongestPalindromSubstring();
 //		String s = "bananas";
-		String s = "babad";
-		System.out.println(a.longestPalindrome_dp(s));
+		String s = "bana";
+		System.out.println(a.solve(s));
 	}
 
-	public String longestPalindrome_dp(String s) {
+	public String solve(String s) {
 		// 1. ds
-		int n = s.length();
-		String res = null;
+		int n = s.length(); // 4
 		boolean[][] dp = new boolean[n][n];
+		String result = null;
 
 		// 2. for
 		for (int i = 0; i < n; i++) {
 			for (int j = i; j >= 0; j--) {
 				System.out.println("dp[" + j + "][" + i + "] " + dp[j][i]);
-				if (s.charAt(i) == s.charAt(j) && (i - j < 2 || dp[j + 1][i - 1])) {
-					dp[j][i] = true;
-					System.out.println("222 dp[" + j + "][" + i + "] " + dp[j][i]);
-					if (res == null || i - j + 1 > res.length()) {
-						res = s.substring(j, i + 1);
-					}
+			    if(s.charAt(i) == s.charAt(j) && (i - j < 2 || dp[j + 1][i - 1])) {
+				   dp[j][i] = true;
+				   
+				   if(result ==null || i-j+1 > result.length()) {
+					   result= s.substring(j, i+1);
+				   }
 				}
 			}
+			System.out.println("");
 		}
+		return result;
 
-		return res;
+	}
+	
+
+	public static void boolprint(boolean[][] arr2) {
+		for (int i = 0; i < arr2.length; i++) {
+			for (int j = 0; j < arr2[i].length; j++) {
+				System.out.print("[" + i + "][" + j + "]" + arr2[i][j]);
+			}
+			System.out.println("");
+		}
 	}
 
 }
