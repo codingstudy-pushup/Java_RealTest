b359eebfd56396fc118abd4e9f94c27c4f32eab0
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-05-18 09:48:09 +0900

    Update A04_ConcatenatedWords.java

--- 02/A04_ConcatenatedWords.java
+++ 02/A04_ConcatenatedWords.java
@@ -12,19 +12,17 @@ public class A04_ConcatenatedWords {
 	}
 
 	public List<String> solve(String[] words) {
+		// 1. ds
 		List<String> result = new ArrayList<>();
 		Set<String> set = new HashSet<>();
-//		Arrays.sort(words, new Comparator<String>() {
-//			public int compare(String s1, String s2) {
-//				return s1.length() - s2.length();
-//			}
-//		});
+
+		// 길이 오름차순
 		Arrays.sort(words, (a, b) -> a.length() - b.length());
 
+		// 2. for, while
 		for (int i = 0; i < words.length; i++) {
+			// cat, dog..
 			if (makeWord(words[i], set)) {
-				System.out.println("word[" + i + "] " + words[i] + " set " + set);
-				System.out.println("");
 				result.add(words[i]);
 			}
 			set.add(words[i]);
@@ -32,24 +30,22 @@ public class A04_ConcatenatedWords {
 		return result;
 	}
 
-	private static boolean makeWord(String word, Set<String> dict) {
-		if (dict.isEmpty())
+	private static boolean makeWord(String word, Set<String> set) {
+
+		if (set.isEmpty())
 			return false;
 		boolean[] dp = new boolean[word.length() + 1];
 		dp[0] = true;
+
 		for (int i = 1; i <= word.length(); i++) {
 			for (int j = 0; j < i; j++) {
-				System.out.println("11 dp[" + j + "] " + dp[j]);
-				if (!dp[j]) {
-					System.out.println("22 dp[" + j + "] " + dp[j]);
-					continue;
-				}
-				System.out.println("word.substring(" + j + " " + i + " ): " + word.substring(j, i));
-				if (dict.contains(word.substring(j, i))) {
-
+				if (dp[j] && set.contains(word.substring(j, i))) {
+					System.out.println("11 dp["+j+"] " + dp[j]);
+					System.out.println("word.substring(" + j + " " + i + ") " + word.substring(j, i));
 					dp[i] = true;
 					break;
 				}
+
 			}
 		}
 		return dp[word.length()];
